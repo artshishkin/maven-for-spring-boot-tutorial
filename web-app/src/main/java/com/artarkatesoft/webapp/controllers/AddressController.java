449600371c5ebb717928d4a57a8798cc08f74ec6
Art <d.art.shishkin@gmail.com>
2020-06-28 22:22:24 +0300

    my solution of assignment

--- web-app/src/main/java/com/artarkatesoft/webapp/controllers/AddressController.java
+++ web-app/src/main/java/com/artarkatesoft/webapp/controllers/AddressController.java
@@ -1,5 +1,6 @@
 package com.artarkatesoft.webapp.controllers;
 
+import com.artarkatesoft.webmodel.ShippingAddess;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AddressController {
 
     @GetMapping("/")
-    public String getAddress() {
-        return "hello";
+    public ShippingAddess getAddress() {
+        ShippingAddess shippingAddess = new ShippingAddess();
+        shippingAddess.setCountryName("Ukraine");
+        shippingAddess.setPostOfficeBox("post office box");
+        shippingAddess.setPostalCode("84300");
+        shippingAddess.setStreetAddress("MyFavourite Street");
+        shippingAddess.setRegion("Donetsk region");
+        return shippingAddess;
     }
 }
